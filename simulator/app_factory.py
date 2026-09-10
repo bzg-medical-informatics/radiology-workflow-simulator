2c2881f2cb828db4539f1e8ca127196baca080fd
Kevin Bohn <kevin.bohn@protonmail.com>
2026-09-11 00:06:03 +0200

    Resolve conflict with upload progress UI

--- simulator/app_factory.py
+++ simulator/app_factory.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from flask import Flask
+from flask import Flask, request
 
 try:
     from simlib import storage
@@ -55,5 +55,19 @@ def create_app() -> Flask:
         if endpoint.endswith('.pacs_instance_preview'):
             app.view_functions[endpoint] = pacs_instance_preview_with_orthanc_fallback
             break
+
+    @app.after_request
+    def add_modality_upload_progress_script(response):
+        """Load the upload progress helper only on the CT modality page."""
+        if (
+            request.path == '/modality'
+            and response.mimetype == 'text/html'
+            and not response.is_streamed
+        ):
+            html = response.get_data(as_text=True)
+            script_tag = '<script src="/static/upload-progress.js"></script>'
+            if script_tag not in html and '</body>' in html:
+                response.set_data(html.replace('</body>', f'{script_tag}\n</body>', 1))
+        return response
 
     return app
